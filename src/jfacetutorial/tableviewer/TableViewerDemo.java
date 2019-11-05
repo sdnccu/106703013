ea5b456c996b44eb45396205c7c84d8e5aad01a5
sdnccu <57346523+sdnccu@users.noreply.github.com>
2019-11-05 10:29:13 +0800

    jfacetutorial

---
+++ src/jfacetutorial/tableviewer/TableViewerDemo.java
@@ -0,0 +1,81 @@
+package jfacetutorial.tableviewer;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.widgets.TableColumn;
+
+import java.util.List;
+import jfacetutorial.Article;
+import jfacetutorial.DataModel;
+
+
+public class TableViewerDemo {
+
+	protected Shell shell;
+	private Table table;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			TableViewerDemo window = new TableViewerDemo();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		shell.setLayout(new FillLayout(SWT.HORIZONTAL));
+		
+		TableViewer tableViewer = new TableViewer(shell, SWT.BORDER | SWT.FULL_SELECTION);
+		
+		ArticleTableCLProvider provider = new ArticleTableCLProvider();        
+		tableViewer.setContentProvider(provider);        
+		tableViewer.setLabelProvider(provider);  
+		
+		table = tableViewer.getTable();
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		
+		TableColumn tblclmnNewColumn = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn.setWidth(100);
+		tblclmnNewColumn.setText("Article");
+		
+		TableColumn tblclmnNewColumn_1 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_1.setWidth(100);
+		tblclmnNewColumn_1.setText("Author");
+
+		List<Article> articleList= DataModel.getArticles();
+		tableViewer.setInput(articleList);
+		
+	}
+
+}
